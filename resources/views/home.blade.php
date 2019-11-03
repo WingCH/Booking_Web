d0a2244c22161a17ed17493f0d07541df7d55954
WingCH <g1191993@study.ouhk.edu.hk>
2019-11-03 21:16:13 +0800

    format, disable js and use a tag

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,36 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-{{  Debugbar::info($rooms->toArray()) }}
-<div class="container">
-    <div class="row">
-        @foreach ($rooms as $room)
-        <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-            <div class="thumbnail" id="{{ $room->id }}">
-                <img alt="text" class="img-responsive img-rounded " src="{{ $room->backgroundImage }}">
-                    <div class="caption">
-                        <h2 >{{ $room->name }}</h2>
-                        <div class="row">
-                            <div class="col-xs-12 text-center ">
-                                <i class="fa fa-map-marker" aria-hidden="true"></i>
-
-                                <small class="text-muted">{{ $room->address }}</small>
-                                </i>
-
+    {{  Debugbar::info($rooms->toArray()) }}
+    <div class="container">
+        <div class="row">
+            @foreach ($rooms as $room)
+                <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                    <div class="thumbnail" id="{{ $room->id }}">
+                        <a href="/room/{{ $room->id }}">
+                            <img alt="text" class="img-responsive img-rounded " src="{{ $room->backgroundImage }}">
+                            <div class="caption">
+                                <h2>{{ $room->name }}</h2>
+                                <div class="row">
+                                    <div class="col-xs-12 text-center ">
+                                        <i class="fa fa-map-marker" aria-hidden="true"></i>
+                                        <small class="text-muted">{{ $room->address }}</small>
+                                    </div>
+                                </div>
                             </div>
-
-                        </div>
+                        </a>
+                        {{-- </img> --}}
                     </div>
-                {{-- </img> --}}
-            </div>
+                </div>
+            @endforeach
         </div>
-        @endforeach
     </div>
-</div>
 @endsection
 
 @section('js')
-{{-- own js --}}
-<script src="/resources/home/home.js" type="text/javascript">
-</script>
+    {{-- own js --}}
+{{--    <script src="/resources/home/home.js" type="text/javascript">--}}
+{{--    </script>--}}
 @endsection
